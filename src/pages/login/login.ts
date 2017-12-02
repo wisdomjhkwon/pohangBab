504af2c6ab1acfc86440826b3b0bdbaa1f6b2665
zkfbdkf <zkfbdkf@gmail.com>
2017-12-02 23:08:33 +0900

    fixing-login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -9,6 +9,7 @@ import { AuthProvider } from '../../providers/auth/auth';
 import { HomePage } from '../home/home';
 
 import { AngularFireAuth } from 'angularfire2/auth';
+import firebase from 'firebase';
 
 /**
  * Generated class for the LoginPage page.
@@ -33,12 +34,30 @@ export class LoginPage {
   }
 
   LoginWithFB() {
-    this.facebook.login(['email', 'public_profile']).then((response: FacebookLoginResponse) => {
-      this.facebook.api('me?fields=id,name,email,first_name,picture.width(720).height(720).as(picture_large)', []).then(profile => {
-        this.userData = { email: profile['email'], first_name: profile['first_name'], picture: profile['picture_large']['data']['url'], username: profile['name'] };
+    // this.facebook.login(['email', 'public_profile']).then((response: FacebookLoginResponse) => {
+    //   this.facebook.api('me?fields=id,name,email,first_name,picture.width(720).height(720).as(picture_large)', []).then(profile => {
+    //     this.userData = { email: profile['email'], first_name: profile['first_name'], picture: profile['picture_large']['data']['url'], username: profile['name'] };
+    //   })
+    // })
+    this.facebook.login(['email']).then(res => {
+      const fc = firebase.auth.FacebookAuthProvider.credential(res.authResponse.accessToken)
+      firebase.auth().signInWithCredential(fc).then(fs => {
+        // alert("firebase sec")
+        this.facebook.api('me?fields=id,name,email,first_name,picture.width(720).height(720).as(picture_large)', []).then(profile => {
+          this.userData = { email: profile['email'], first_name: profile['first_name'], picture: profile['picture_large']['data']['url'], username: profile['name'] };
+        })
+        this.userEmail=this.userData.email;
+        window.localStorage.setItem('currentuser', this.userEmail);
+        alert(this.userEmail);
+      }).catch(ferr => {
+        alert("login error")
       })
+
+    }).catch(err => {
+      alert(JSON.stringify(err))
     })
   }
+
 
   async login() {
 
