7bc1953bd545ff610cfa7ff4ffdbae67a2d2d7f5
HyunWoo_Kang <0305qwer@naver.com>
2017-12-03 20:38:31 +0900

    Loading, Refreshing

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, Refresher } from 'ionic-angular';
 import { Page5Page } from '../page5/page5';
 import { Page9Page } from '../page9/page9';
 
@@ -10,30 +10,47 @@ import { LoginPage } from '../login/login';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  userEmail: string = null;
+  loader: LoadingController;
+  refresher: Refresher;
 
-  userEmail: string=null;
-
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+    public loadingCtrl: LoadingController) {
     // window.localStorage.removeItem('currentuser');
     if (!this.isLoggedin()) {
       console.log('You are not logged in');
       this.navCtrl.push(LoginPage);
-      
     }
+
+    let loader = this.loadingCtrl.create({
+      content: "Loading location data...",
+      duration: 2000
+    });
+
+    loader.present();
+
   }
 
   isLoggedin() {
     if (window.localStorage.getItem('currentuser')) {
       console.log(window.localStorage.getItem('currentuser'));
-      
+
       return true;
     }
   }
   goToPage5(params) {
     if (!params) params = {};
     this.navCtrl.push(Page5Page);
-  } goToPage9(params) {
+  }
+
+  goToPage9(params) {
     if (!params) params = {};
     this.navCtrl.push(Page9Page);
+  }
+
+  doRefresh(refresher) {
+    setTimeout(() => {
+      refresher.complete();
+    }, 2000);
   }
 }
--- src/pages/page6/page6.ts
+++ src/pages/page6/page6.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController} from 'ionic-angular';
 import { Page5Page } from '../page5/page5';
 import { Page9Page } from '../page9/page9';
 
@@ -14,6 +14,7 @@ export class Page6Page {
   goToPage5(params){
     if (!params) params = {};
     this.navCtrl.push(Page5Page);
+
   }goToPage9(params){
     if (!params) params = {};
     this.navCtrl.push(Page9Page);
