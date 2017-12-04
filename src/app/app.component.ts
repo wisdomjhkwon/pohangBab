07524f281942be258ffe5522f8e069ee7538fb45
shinjh0849 <21200421@handong.edu>
2017-12-05 01:50:51 +0900

    Jiho push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,7 +26,7 @@ import { StationListPage } from '../pages/station-list/station-list';
 export class MyApp {
   @ViewChild(Nav) navCtrl: Nav;
   
-  rootPage: any = Page9Page;
+  rootPage: any = StationListPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public reviewData: ReviewData, private auth: AngularFireAuth) {
       
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -92,6 +92,7 @@ export class HomePage {
 
   ionViewDidLoad() {
     this.loadMap();
+    
   }
 
   loadMap() {
